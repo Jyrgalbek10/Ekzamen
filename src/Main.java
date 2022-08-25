40844a06435beafce80a19d58af38f5b9e6f1d17
Jyrgalbek10 <110243095+Jyrgalbek10@users.noreply.github.com>
2022-08-25 10:26:09 +0300

    commit tula

--- src/Main.java
+++ src/Main.java
@@ -2,30 +2,31 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("a");
-        System.out.println("2");
-        System.out.println("2");
-            People[] people1 = {
-                    new Asan("Adam", "Asan", "Asanov", "asan@mail.com", "osh", 19),
-                    new Uson("A", "Uson", "Usonov", "uson@mail", "Bishkek", 20),
-                    new Adyl("B", "Adyl", "Adylov", "adyl@mail", "Moskva", 21),
-                    new Akyl("V", "Akyl", "Akylov", "akyl@mail", "Kyzyl", 22),
-                    new Ainur("G", "Ainur", "Ainurovna", "ainur@mail", "London", 23)
+        Asan asan = new Asan("Adam","Asan","Asanov", "asan@mail.com", "osh",19);
+        asan.getEmail();
+//        if (asan.getEmail().contains("@")){
+//            System.out.println("Tuura");
+//
+//        }else {
+//            System.out.println("error");
 
-            };
-        for (int i = 0; i < 5; i++) {
-            
+        Uson uson = new Uson("A","Uson","Usonov","uson@mail","Bishkek",20);
+        Adyl adyl = new Adyl("B","Adyl","Adylov","adyl@mail","Moskva",21);
+        Akyl akyl = new Akyl("V","Akyl","Akylov","akyl@mail","Kyzyl",22);
+        Ainur ainur = new Ainur("G","Ainur","Ainurovna","ainur@mail","London",23);
+
+        People[] people1={
+                new Asan("Adam","Asan","Asanov", "asan@mail.com", "osh",19),
+                new Uson("A","Uson","Usonov","uson@mail","Bishkek",20),
+                new Adyl("B","Adyl","Adylov","adyl@mail","Moskva",21),
+                new Akyl("V","Akyl","Akylov","akyl@mail","Kyzyl",22),
+                new Ainur("G","Ainur","Ainurovna","ainur@mail","London",23)
+
+        };
+        for (People p:people1) {
+            System.out.println(p);
+
         }
-
-            for (People p : people1) {
-                System.out.println(p);
-                if (p.getEmail().contains("@")) {
-                    System.out.println("Tuura");
-                } else {
-                    System.out.println("error");
-                }
-        }
-
     }
+
 }
